1028888fea0a76062f9c33d648847b9ab0bcda5b
Michael Hegner <michaelhegner77@gmail.com>
2020-12-29 15:25:27 +0100

    Base Configuration

---
+++ src/main/java/net/hemisoft/be/robin/meetandlearn/Main.java
@@ -0,0 +1,13 @@
+package net.hemisoft.be.robin.meetandlearn;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String hello = new Main().sayHello();
+		System.out.println(hello);
+	}
+	
+	public String sayHello() {
+		return "Hello";
+	}
+}
